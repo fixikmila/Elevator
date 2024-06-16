11bebe8b4d1ac6ccbf0f5689872bec2c6d9841eb
fixikmila <99081680+fixikmila@users.noreply.github.com>
2024-06-16 17:02:30 +0200

    Delete TupleFloor.java

--- TupleFloor.java
+++
@@ -1,9 +0,0 @@
-public class TupleFloor {
-    int current_floor, destination_floor, time_call;
-
-    public TupleFloor(int current_floor, int destination_floor, int time_call) {
-        this.current_floor = current_floor;
-        this.destination_floor = destination_floor;
-        this.time_call = time_call;
-    }
-}
